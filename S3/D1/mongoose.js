72059cc2052ae65c8860217c5c5bd47207b480d4
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-04-01 14:58:56 +0530

    conneted to db, structure and validation

--- S3/D1/mongoose.js
+++ S3/D1/mongoose.js
@@ -1,1 +1,36 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose");
+
+const main = async () => {
+  try {
+    const connection = await mongoose.connect(
+      "mongodb://127.0.0.1:27017/backend"
+    );
+
+    console.log("Connected to DB");
+    // await StudentModel.insertMany([{ name: "chunnu", age: 29, city: "Patna" }]);
+    const student = new StudentModel({
+      name: "56",
+      age: 30,
+      city: "Bengaluru",
+    });
+    await student.save();
+
+    console.log(student);
+  } catch (error) {
+    console.log(error);
+  }
+};
+main();
+
+const studentSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    age: { type: Number, required: true },
+    city: { type: String, required: true },
+  },
+  {
+    versionKey: false,
+  }
+);
+
+const StudentModel = mongoose.model("student", studentSchema);
