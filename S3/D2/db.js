1ede51c1313477dc9249ab91142e64d7cb9c6f0f
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-04-02 10:04:42 +0530

    crud operation and connected to mongodb atlas

--- S3/D2/db.js
+++ S3/D2/db.js
@@ -1,5 +1,22 @@
 const mongoose = require("mongoose");
 
-const connection = mongoose.connect("mongodb://127.0.0.1:27017/university");
+const connection = mongoose.connect(
+  "mongodb+srv://aman:amankr@cluster0.efrkmbv.mongodb.net/university?retryWrites=true&w=majority&appName=Cluster0"
+);
 
-module.exports = { connection };
+const userSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    age: { type: Number, required: true },
+    city: { type: String, required: true },
+    language: { type: String, required: true },
+    isMarried: { type: Boolean, required: true },
+  },
+  {
+    versionKey: false,
+  }
+);
+
+const UserModel = mongoose.model("user", userSchema);
+
+module.exports = { connection, UserModel };
--- S3/D2/index.js
+++ S3/D2/index.js
@@ -1,14 +1,52 @@
 const express = require("express");
-const { connection } = require("./db");
+const { connection, UserModel } = require("./db");
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Welcome Home Page");
 });
 
-app.get("/users", (req, res) => {
-  res.send("All the users data will be send");
+app.get("/users", async (req, res) => {
+  const query = req.query;
+  try {
+    const allUsers = await UserModel.find(query);
+    res.send(allUsers);
+  } catch (error) {
+    res.send({ msg: "cannot get users", error: error.message });
+  }
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    const user = new UserModel(req.body);
+    await user.save();
+    res.send("user has been registered");
+  } catch (error) {
+    res.send({ msg: "cannot registered", error: error.message });
+  }
+});
+
+app.patch("/update/:id", async (req, res) => {
+  const ID = req.params.id;
+  const payload = req.body;
+  try {
+    await UserModel.findByIdAndUpdate({ _id: ID }, payload);
+    res.send("user has been updated");
+  } catch (error) {
+    res.send({ msg: "cannot update successfully", error: error.message });
+  }
+});
+
+app.delete("/delete/:id", async (req, res) => {
+  const ID = req.params.id;
+  try {
+    await UserModel.findByIdAndDelete({ _id: ID });
+    res.send("user has been deleted");
+  } catch (error) {
+    res.send({ msg: "cannot deleted successfully", error: error.message });
+  }
 });
 
 app.listen(5050, async () => {
